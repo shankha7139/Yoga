ce0d64502753eaa88f4c0e0217bcc7ba5b76c021
shankha7139 <shankha7139@gmail.com>
2024-05-24 19:11:13 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import React from 'react'
-import Hero from './Components/Hero'
-import Navbar from './Components/Navbar'
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Floating from './Components/Floating'
-import Members from './Components/Members'
-
-export default function App() {
+const App = () => {
   return (
-    <div className='bg-slate-200' >
-      <Navbar/>
-      <div className='bg-black text-white flex justify-center item-center gap-6 ' > 
-        <p>
-          samplemail@gmail.com
-        </p>
-        <p>
-          69696969696
-        </p>
-        <p className='text-white/50 ease-in-out duration-300  hover:text-white cursor-pointer ' >
-          Become a member
-        </p>
-        <p className='text-white/50 ease-in-out duration-300  hover:text-white cursor-pointer ' >
-          Contact
-        </p>
-      </div>
-      <Hero/>
-      <Floating/>
-      <Members/>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/about' element={<About/>}  />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import Navbar from '../Components/Navbar'
+import Floating from '../Components/Floating'
+import Members from '../Components/Members'
+
+export default function App() {
+  return (
+    <div className='bg-slate-200' >
+      <Navbar/>
+      <div className='bg-black text-white flex justify-center item-center gap-6 ' > 
+        <p>
+          samplemail@gmail.com
+        </p>
+        <p>
+          69696969696
+        </p>
+        <p className='text-white/50 ease-in-out duration-300  hover:text-white cursor-pointer ' >
+          Become a member
+        </p>
+        <p className='text-white/50 ease-in-out duration-300  hover:text-white cursor-pointer ' >
+          Contact
+        </p>
+      </div>
+      <Hero/>
+      <Floating/>
+      <Members/>
+    </div>
+  )
+}
