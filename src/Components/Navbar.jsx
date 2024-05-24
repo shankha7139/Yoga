a2f6d0606dd56829f2576e17f773883a6637194c
shankha7139 <shankha7139@gmail.com>
2024-05-24 20:16:28 +0530

    _

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png"
 export default function Navbar() {
   return (
     <>
-        <div className="fixed z-40 bg-slate-600/50 flex justify-between mt-6 items-center w-full px-8 py-4 backdrop-blur-sm">
+        <div className="fixed z-40 bg-slate-600/50 flex justify-between mt-6 items-center w-full px-8 py-4 backdrop-blur-lg">
             <img src={logo} className='w-20 h-20 ml-8' lt="" />
             <div className="anchors flex gap-10 text-white text-xl items-center text-center pl-28  ">
                 <a href="" className=" underline decoration-solid decoration-0 hover:decoration-2"> Home </a>
